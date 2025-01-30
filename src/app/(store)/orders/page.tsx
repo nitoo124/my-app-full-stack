36b7661695b844711071a01697063e761285e7e2
nitoo124 <neetunagdev258@gmail.com>
2025-01-31 04:50:54 +0500

    updated

--- src/app/(store)/orders/page.tsx
+++ src/app/(store)/orders/page.tsx
@@ -1,26 +1,39 @@
-import { getMyOrders } from '@/sanity/lib/orders/grtMyOrders';
-import { auth } from '@clerk/nextjs/server'
-import { redirect } from 'next/navigation';
+// import { getMyOrders } from '@/sanity/lib/orders/grtMyOrders';
+// import { auth } from '@clerk/nextjs/server'
+// import { redirect } from 'next/navigation';
+// import React from 'react'
+
+//  async function Orders() {
+//     const  {userId} = await auth();
+//      if (!userId) {
+//         return redirect('/')
+        
+//      }
+
+//      const orders = await getMyOrders(userId)
+//   return (
+//     <div>
+//       {orders.map(()=>{
+//         return(
+//             <div></div>
+//         )
+//       })}
+      
+//     </div>
+//   )
+// }
+
+// export default Orders
 import React from 'react'
 
- async function Orders() {
-    const  {userId} = await auth();
-     if (!userId) {
-        return redirect('/')
-        
-     }
-
-     const orders = await getMyOrders(userId)
+function orders() {
   return (
     <div>
-      {orders.map(()=>{
-        return(
-            <div></div>
-        )
-      })}
+      orders
       
     </div>
   )
 }
 
-export default Orders
+export default orders
+
